50007714fa2e6a2b2b0b5db79a077c9f3fbd7333
Gabriel <78756794+gfioreze@users.noreply.github.com>
2025-09-09 10:25:42 +0100

    FizzBuzz

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,13 @@
 <?php
+
+for ($number = 1; $number <= 100; $number++) {
+  if (0 == $number % 15) {
+    echo "FizzBuzz" . PHP_EOL;
+  } elseif (0 == $number % 5) {
+    echo "Buzz" . PHP_EOL;
+  } elseif (0 == $number % 3) {
+    echo "Fizz" . PHP_EOL;
+  } else {
+    echo $number . PHP_EOL;
+  }
+}
